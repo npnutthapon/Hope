bc1e6a4f41eb53c6e929782d8b179358d4716862
npnutthapon <68525346+npnutthapon@users.noreply.github.com>
2020-09-24 14:46:29 +0700

    .

---
+++ pages/shop/index.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <h1>Shop page</h1>
+    <ul>
+        <li v-for="i in 10" :key="i">สินค้าทั้งหมด {{i}}</li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
