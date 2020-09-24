6416d0b96664dee5739863495e5f9dfeb5a37d97
npnutthapon <68525346+npnutthapon@users.noreply.github.com>
2020-09-24 15:49:10 +0700

    .

--- pages/shop/index.vue
+++ pages/shop/index.vue
@@ -1,10 +1,18 @@
 <template>
-<div>
+  <div>
     <h1>Shop page</h1>
     <ul>
-        <li v-for="i in 10" :key="i">สินค้าทั้งหมด {{i}}</li>
+      <li v-for="i in 10" :key="i">
+        สินค้าทั้งหมด {{ i }}
+        <nuxt-link
+          :to="{name:'shop-id',
+                params:{id:i},}"
+        >
+          รายละเอียด
+        </nuxt-link>
+      </li>
     </ul>
-</div>
+  </div>
 </template>
 
 <script>
