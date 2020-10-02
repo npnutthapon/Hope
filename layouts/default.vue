a1e2f6e829a885eac2a0965e9f434af667070bfc
npnutthapon <68525346+npnutthapon@users.noreply.github.com>
2020-10-02 18:37:56 +0700

    .

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,22 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
+  <v-app
+    dark
+  >
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
       <v-list>
-        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -14,20 +28,8 @@
     </v-navigation-drawer>
     <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? "right" : "left"}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <nuxt-link to="/">
-        Welcome
-      </nuxt-link>
       <v-btn />
     </v-app-bar>
     <v-main>
@@ -60,31 +62,22 @@ export default {
       clipped: false,
       drawer: false,
       fixed: false,
-      items: [{
-        icon: 'mdi-apps',
-        title: 'Welcome',
-        to: '/'
-      },
-      {
-        icon: 'mdi-chart-bubble',
-        title: 'Inspire',
-        to: '/inspire'
-      },
-      {
-        icon: 'mdi-account-circle',
-        title: 'Profile',
-        to: '/profile'
-      },
-      {
-        icon: 'mdi-shopping',
-        title: 'Shop',
-        to: '/shop'
-      }
+      items: [
+        {
+          icon: 'mdi-apple',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-play-circle',
+          title: 'iTunesMusic',
+          to: '/product'
+        }
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Welcome to iTunes'
     }
   }
 }
